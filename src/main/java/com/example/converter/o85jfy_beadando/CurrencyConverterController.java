b5fa9bdaef09d5e0f8c350f224b55b6339b14360
a-stefi <stefanalexandrosz7@gmail.com>
2025-01-07 00:01:13 +0100

    Alapvető konvertáló logika váza

--- src/main/java/com/example/converter/o85jfy_beadando/CurrencyConverterController.java
+++ src/main/java/com/example/converter/o85jfy_beadando/CurrencyConverterController.java
@@ -1,5 +1,60 @@
-package com.example.converter;
+package com.example.atm.o85jfy_beadando;
 
-public class CurrencyConverter {
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CurrencyConverterController {
+
+    @FXML
+    private TextField inputAmount;
+
+    @FXML
+    private ComboBox<String> fromCurrencyComboBox;
+
+    @FXML
+    private ComboBox<String> toCurrencyComboBox;
+
+    @FXML
+    private Label resultLabel;
+
+    private final Map<String, Double> exchangeRates = new HashMap<>();
+
+    private static final List<String> allCurrencies = new ArrayList<>();
+
+    private List<String> favoriteCurrencies = new ArrayList<>();
+
+    @FXML
+    public void initialize() {
+        addCurrencies();
+        updateComboBoxes();
+    }
+
+    private void addCurrencies() {
+    }
+
+    @FXML
+    private void handleConvertButtonAction() {
+    }
+
+    @FXML
+    private void handleSettingsButtonAction() {
+    }
+
+    public void setFavoriteCurrencies(List<String> favorites) {
+    }
+
+    private void updateComboBoxes() {
+    }
+
+    public void printFavoriteCurrencies() {
+    }
 }
