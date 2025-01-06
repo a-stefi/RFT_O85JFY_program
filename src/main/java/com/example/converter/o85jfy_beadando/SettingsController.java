df9c06a4a923b09d590b5eec6716a95ddd433f09
a-stefi <stefanalexandrosz7@gmail.com>
2025-01-07 00:03:23 +0100

    Beállítások ablak kódjának váza

--- src/main/java/com/example/converter/o85jfy_beadando/SettingsController.java
+++ src/main/java/com/example/converter/o85jfy_beadando/SettingsController.java
@@ -11,6 +11,35 @@ import java.util.List;
 
 public class SettingsController {
 
-}
+    @FXML
+    private ListView<String> currencyListView;
 
-   
+    private List<String> availableCurrencies = new ArrayList<>();
+    private List<String> selectedCurrencies = new ArrayList<>();
+
+    private CurrencyConverterController currencyConverterController;
+
+    public void setCurrencyConverterController(CurrencyConverterController controller) {
+        this.currencyConverterController = controller;
+    }
+
+    @FXML
+    public void initialize() {
+        currencyListView.setCellFactory(lv -> new ListCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    CheckBox checkBox = new CheckBox(item);
+                    checkBox.setSelected(selectedCurrencies.contains(item));
+                    checkBox.setOnAction(e -> handleCurrencySelection(checkBox, item));
+
+                    HBox hBox = new HBox(checkBox);
+                    setGraphic(hBox);
+                }
+            }
+        });
+    }
